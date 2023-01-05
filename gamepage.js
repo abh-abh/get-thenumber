eb9b8f0638bb69774ccdcb1bb55aa896c29ae356
abh-abh <94828245+abh-abh@users.noreply.github.com>
2023-01-05 17:50:40 +0530

    Add files via upload

---
+++ gamepage.js
@@ -0,0 +1,25 @@
+var y = Math.floor(Math.random() * 10 + 1);
+  
+var x = document.getElementById("guessField").vlaue;
+
+if(x == y)
+{
+    alert("CONGRATULATIONS"+playername+"  YOU GUESSED IT RIGHT IN "
+            + guess + " Guess ");
+    guess= 1;
+}
+
+else if(x > y)
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+    guess++;
+    alert("OOPS SORRY!! TRY A GREATER NUMBER");
+}
+
+function playAgain(){
+    y = Math.floor(Math.random() * 10 + 1); 
+}
